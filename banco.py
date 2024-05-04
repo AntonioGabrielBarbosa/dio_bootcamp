20cbae974a5cf95937641c767d0e4471ed0e6345
AntonioGabrielBarbosa <antonio.gabrielbarbosa2003@hotmail.com>
2024-05-04 18:35:04 -0300

    criacao do banco

---
+++ banco.py
@@ -0,0 +1,20 @@
+
+
+while True:
+
+
+    menu = """
+
+    SISTEMA DE BANCO SIMPLES
+
+    [d] - depósito
+    [s] - saque
+    [e] - extrar
+    [sair] - sair
+
+"""
+
+    opcao = input("\nEscolha sua opcao: ")
+
+    deposito = 0
+    LIMITE_SAQUE = 500
